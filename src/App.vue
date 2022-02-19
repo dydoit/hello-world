4385675257c7f378d5e30c7f57ee5647adb17b1a
dengy29 <18565125218@163.com>
2022-02-19 17:45:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,9 @@
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
 nav {
